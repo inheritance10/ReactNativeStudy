3ed4ad4afa1367fcebf7ff8b5124d4d23d6be480
inheritance10 <ali552001a@gmail.com>
2022-11-05 04:28:52 +0300

    stack navigation tamamlandi

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
+import React, {Component} from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createAppContainer } from 'react-navigation';
+import {createStackNavigator} from "react-navigation-stack";
+
+
+class App extends Component {
+  render() {
+    return (
+          <View style={styles.container}>
+            <Text>HOME PAGE</Text>
+            <TouchableOpacity style={{padding:8 ,backgroundColor:'pink',borderRadius:8}} onPress={() => this.props.navigation.navigate('Detail')}>
+              <Text>Detay sayfasına git</Text>
+            </TouchableOpacity>
+          </View>
+    );
+  }
 }
+
+class Detail extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>DETAIL PAGE</Text>
+        </View>
+    );
+  }
+}
+
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: App
+  },
+  Detail: {
+    screen: Detail
+  }
+}, {
+    initialRouteName: 'Home' //başlangıçta hangi sayfa gözüksün
+});
+
+
+
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
